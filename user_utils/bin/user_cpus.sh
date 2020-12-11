1cb4988be4dd8451c7c71fb6c6b8071c32d028c6
Jordan Hayes <hayesjordan@gmail.com>
2020-12-11 10:10:44 -0800

    Added short part

--- user_utils/bin/user_cpus.sh
+++ user_utils/bin/user_cpus.sh
@@ -7,7 +7,7 @@ module load slurm/19.05.0
 
 echo -e "You ($USER) are using the following CPU cores per partition:"
 
-for PART in intel batch highmem gpu; do
+for PART in short intel batch highmem gpu; do
     USED=$(squeue -u $USER -p $PART -o '%C' -t R -h | tr '\n' '+')0 
     echo -e "\t\e[32m$PART\t\e[33m$(echo ${USED} | bc)\e[0m"
 done
