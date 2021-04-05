87c4bd9278d20d21167e994aa265ac186e9b764d
Jordan Hayes <hayesjordan@gmail.com>
2021-04-05 16:48:48 -0700

    Print entire user name

--- bin/slurm_limits.sh
+++ bin/slurm_limits.sh
@@ -9,7 +9,7 @@ echo -e "==============="
 
 #for LIMIT in $(sacctmgr show account $SGROUP format=partition,grptres%30 --ass --noheader | sed "s/$SGROUP.*//g" | sed "s/\sgpu.*//g" | sort | uniq); do
 #for LIMIT in $(sacctmgr show account $SGROUP format=partition,grptres%30 --ass --noheader | sed "s/\sgpu.*//g" | sort | uniq); do
-for LIMIT in $(sacctmgr show account $SGROUP format=partition,grptres%30,user --ass --noheader | grep -P "$USER\s*$|gpu\=" | sed "s/\s*$USER\s*//g" | grep -v '^\s*gpu\s*$' | sort | uniq); do
+for LIMIT in $(sacctmgr show account $SGROUP format=partition,grptres%30,user%30 --ass --noheader | grep -P "$USER\s*$|gpu\=" | sed "s/\s*$USER\s*//g" | grep -v '^\s*gpu\s*$' | sort | uniq); do
     TRIM_LIMIT=$(echo $LIMIT | sed 's/ //g')
     echo $TRIM_LIMIT | grep 'gres' &> /dev/null
     EXIT=$?
