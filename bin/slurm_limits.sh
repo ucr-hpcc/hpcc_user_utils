9fe12004b4b3361b6426df0d15e1ff7b420148a2
Jordan Hayes <hayesjordan@gmail.com>
2022-04-25 17:18:38 -0700

    New format for limits

--- bin/slurm_limits.sh
+++ bin/slurm_limits.sh
@@ -7,18 +7,19 @@ echo -e "==============="
 echo "Slurm Limits"
 echo -e "==============="
 
-echo -e "\tGroup Limits"
-echo -e "\t---------------"
 for LIMIT in $(sacctmgr show account $SGROUP format=grptres%30 --ass --noheader | grep "gpu=" | sort | uniq); do
-    TRIM_LIMIT=$(echo $LIMIT | sed 's/ //g')
-    echo -e "\n\tALL\t\t$TRIM_LIMIT"
+    SLURM_LIMITS="Group ALL $(echo $LIMIT | sed 's/ //g') N/A"
 done
     
-echo -e "\n\tUser Limits"
-echo -e "\t---------------"
-for LIMIT in $(sacctmgr show user $USER format=partition,grptres%30 --ass --noheader | sort | uniq); do
+for LIMIT in $(sacctmgr show user $USER format=partition,grptres%30,maxsubmit --ass --noheader | sort | uniq); do
     PART=$(echo $LIMIT | awk '{print $1}' | sed 's/ //g')
     TRES=$(echo $LIMIT | awk '{print $2}' | sed 's/ //g')
-    echo -e "\t$PART\t\t$TRES"
+    MAX=$(echo $LIMIT | awk '{print $3}' | sed 's/ //g')
+    if [[ $(echo $PART | grep -P '^[a-z]') ]] && [[ -n $MAX ]]; then
+       SLURM_LIMITS=$(echo -e "$SLURM_LIMITS\nUser $PART $TRES $MAX")
+    elif [[ $(echo $PART | grep -P '^[a-z]') ]] && [[ -z $MAX ]]; then
+       SLURM_LIMITS=$(echo -e "$SLURM_LIMITS\nUser $PART N/A $TRES") 
+    fi
 done
 
+echo -e "$SLURM_LIMITS" | column -t --table-columns TYPE,PARTITION,RESOURCE,MAXSUBMIT
